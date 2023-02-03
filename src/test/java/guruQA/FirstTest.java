2c0b9ad90788174532e9356b17d6b5032e115aa1
Льватов Юрий Михайлович <yuriy.lvatov@rt.ru>
2023-02-03 23:45:09 +0300

    first commit

---
+++ src/test/java/guruQA/FirstTest.java
@@ -0,0 +1,11 @@
+package guruQA;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+    @Test
+    void firstTest() {
+        Assertions.assertTrue(3 > 2);
+    }
+}
